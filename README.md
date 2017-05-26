24a9ef7ae38e311a35dce89cf43d9ba6dc515422
koval <r#cartoon1>
2017-05-26 12:31:20 +0300

    d-space. s-e. Records.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Minimal Apache Spark project.
 
 ## Links
 * [How to run Apache Spark on Windows7 in standalone mode](http://nishutayaltech.blogspot.ru/2015/04/how-to-run-apache-spark-on-windows7-in.html)
+* [Generator](https://www.mockaroo.com/)
--- s-e/src/main/java/m/d/s/ex/e/Main.java
+++ s-e/src/main/java/m/d/s/ex/e/Main.java
@@ -1,24 +1,72 @@
 package m.d.s.ex.e;
 
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
-import scala.Tuple2;
+import org.apache.spark.api.java.function.Function;
 
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        final SparkConf conf = new SparkConf().setMaster("local").setAppName("Minimal");
+    public static void main(final String[] args) {
+        final SparkConf conf = new SparkConf().setMaster("local").setAppName("e");
         try (JavaSparkContext sc = new JavaSparkContext(conf)) {
-            final JavaRDD<String> textFile = sc.textFile("s-m/src/main/resources/data.txt");
-            final JavaPairRDD<String, Integer> counts = textFile
-                    .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
-                    .mapToPair(word -> new Tuple2<>(word, 1))
-                    .reduceByKey((a, b) -> a + b);
-            counts.foreach(p -> System.out.println(p));
-            System.out.println("Total words: " + counts.count());
+            final List<Record> records = Arrays.asList(
+                    new Record("Shayna Dalwood", "1990"),
+                    new Record("Talya Aysh", "1991"),
+                    new Record("Taly Aysh", "1991"),
+                    new Record("Bethany Euston", "1989"),
+                    new Record("Bethany Eustonn", "1989")
+            );
+            final JavaRDD<Record> source = sc.parallelize(records);
+            final Executor executor = new Executor();
+            executor.execute(source);
+        }
+    }
+
+    private static class Executor {
+        void execute(final JavaRDD<Record> source) {
+            source.map((Function<Record, Record>) record -> {
+                System.out.println(record);
+                return record;
+            }).count();
+        }
+    }
+
+    private static class Record implements Serializable {
+        final String name;
+        final String date;
+
+        Record(final String name, final String date) {
+            this.name = name;
+            this.date = date;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            final Record record = (Record) o;
+
+            return name.equals(record.name) && date.equals(record.date);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + date.hashCode();
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Record{" +
+                    "name='" + name + '\'' +
+                    ", date='" + date + '\'' +
+                    '}';
         }
     }
 }
