d9d203f57ce243a605cb010c52fff083ad48e532
kartzum <kartzum@gmail.com>
2017-05-27 00:35:41 +0300

    d-space. s-e. Features.

--- s-e/src/main/java/m/d/s/ex/e/Main.java
+++ s-e/src/main/java/m/d/s/ex/e/Main.java
@@ -1,43 +1,68 @@
 package m.d.s.ex.e;
 
-import org.apache.commons.codec.language.bm.BeiderMorseEncoder;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.Function;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.StringEncoder;
+import org.apache.commons.codec.language.Metaphone;
+import org.apache.spark.api.java.function.ForeachFunction;
+import org.apache.spark.ml.feature.*;
+import org.apache.spark.ml.linalg.SparseVector;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.*;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    final static BeiderMorseEncoder beiderMorseEncoder = new BeiderMorseEncoder(); // TODO. Solve problem with Serializable.
+    private final static StringEncoder stringEncoder = new Metaphone(); // TODO. Solve problem with Serializable.
 
     public static void main(final String[] args) {
 
-        final SparkConf conf = new SparkConf().setMaster("local").setAppName("e");
-        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+        try (SparkSession spark = SparkSession.builder()
+                .appName("s-e")
+                .master("local[2]")
+                .getOrCreate()) {
+
+            // TODO. Extract data.
+
             final List<Record> records = Arrays.asList(
                     new Record("Shayna Dalwood", "1990"),
                     new Record("Talya Aysh", "1991"),
                     new Record("Taly Aysh", "1991"),
+                    new Record("Aysh Talya ", "1991"),
                     new Record("Bethany Euston", "1989"),
-                    new Record("Bethany Eustonn", "1989")
+                    new Record("Bethany Eustonn", "1989"),
+                    new Record("Euston Bethany", "1989")
             );
-            final JavaRDD<Record> source = sc.parallelize(records); // TODO. Read data from remote source.
-            final Executor executor = new Executor();
-            executor.execute(source);
-        }
-    }
 
-    private static class Executor {
-        void execute(final JavaRDD<Record> source) {
-            source.map((Function<Record, RecordEx>) record -> {
-                final String data = beiderMorseEncoder.encode(record.name);
-                final RecordEx result = new RecordEx(record.name, record.date, data);
-                System.out.println(result);
-                return result;
-            }).count();
+            final List<Row> data = new ArrayList<>();
+
+            records.forEach((Record a) -> {
+                try {
+                    final String[] rawNames = a.name.split("\\s");
+                    final List<String> calculatedNames = new ArrayList<>();
+                    final List<String> parseNames = new ArrayList<>();
+                    for (String rName : rawNames) {
+                        final String d = stringEncoder.encode(rName);
+                        final String[] rawElements = d.split("\\|");
+                        Collections.addAll(calculatedNames, rawElements);
+                        parseNames.add(rName);
+                    }
+                    data.add(RowFactory.create(a.name, a.date, parseNames, calculatedNames));
+                } catch (EncoderException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            final StructType schema = new StructType(new StructField[]{
+                    new StructField("name", DataTypes.StringType, true, Metadata.empty()),
+                    new StructField("date", DataTypes.StringType, true, Metadata.empty()),
+                    new StructField("parseNames", new ArrayType(DataTypes.StringType, true), false, Metadata.empty()),
+                    new StructField("calculatedNames", new ArrayType(DataTypes.StringType, true), false, Metadata.empty()),
+            });
+
+            final Dataset<Row> dataSet = spark.createDataFrame(data, schema);
+
+            new Computer().compute(dataSet);
         }
     }
 
@@ -51,24 +76,21 @@ public class Main {
         }
     }
 
-    private static class RecordEx implements Serializable {
-        final String name;
-        final String date;
-        final String data;
+    private static class Computer {
+        void compute(final Dataset<Row> dataSet) {
 
-        RecordEx(final String name, final String date, final String data) {
-            this.name = name;
-            this.date = date;
-            this.data = data;
-        }
+            final HashingTF hashingTF = new HashingTF()
+                    .setInputCol("calculatedNames")
+                    .setOutputCol("pFeatures")
+                    .setNumFeatures(30);
 
-        @Override
-        public String toString() {
-            return "RecordEx{" +
-                    "name='" + name + '\'' +
-                    ", date='" + date + '\'' +
-                    ", data='" + data + '\'' +
-                    '}';
+            final Dataset<Row> fData = hashingTF.transform(dataSet);
+
+            fData.foreach((ForeachFunction<Row>) row -> {
+                final String name = row.getAs("name");
+                final SparseVector pFeatures = row.getAs("pFeatures");
+                System.out.println(name + " " + pFeatures);
+            });
         }
     }
 }
