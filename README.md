05075dc27ce552ed1866d540651d410118d9ccf6
koval <r#cartoon1>
2017-05-29 17:58:26 +0300

    d-space. s-e. Edges.

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Prototype of searching duplicate data in text rows (Spark).
 Small examples (Spark).
 * [Bucketed Random Projection for Euclidean Distance](https://spark.apache.org/docs/latest/ml-features.html#bucketed-random-projection-for-euclidean-distance)
 * [How to do a self cartesian product over the different partitions of a Spark dataset?](https://stackoverflow.com/questions/43280500/how-to-do-a-self-cartesian-product-over-the-different-partitions-of-a-spark-data)
+* [Join Operators](https://jaceklaskowski.gitbooks.io/mastering-apache-spark/content/spark-sql-joins.html)
+* [Join Operators - Java](https://github.com/high-performance-spark/high-performance-spark-examples/blob/master/src/main/java/com/highperformancespark/examples/dataframe/JavaHappyPandas.java)
 
 ## Links
 * [How to run Apache Spark on Windows7 in standalone mode](http://nishutayaltech.blogspot.ru/2015/04/how-to-run-apache-spark-on-windows7-in.html)
--- s-e/src/main/java/m/d/s/ex/e/Main.java
+++ s-e/src/main/java/m/d/s/ex/e/Main.java
@@ -3,37 +3,40 @@ package m.d.s.ex.e;
 import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.StringEncoder;
 import org.apache.commons.codec.language.Metaphone;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.graphx.Edge;
 import org.apache.spark.graphx.EdgeDirection;
 import org.apache.spark.graphx.Graph;
 import org.apache.spark.graphx.GraphLoader;
 import org.apache.spark.ml.feature.BucketedRandomProjectionLSH;
 import org.apache.spark.ml.feature.BucketedRandomProjectionLSHModel;
 import org.apache.spark.ml.feature.HashingTF;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
 import org.apache.spark.sql.types.*;
 import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     private final static StringEncoder stringEncoder = new Metaphone(); // TODO. Solve problem with Serializable.
 
     public static void main(final String[] args) {
 
+        // System.setProperty("hadoop.home.dir", "d:\\U\\G1\\T\\spark\\spark-2.1.1-bin-hadoop2.7\\"); // TODO. Calculate in runtime.
+
         try (SparkSession spark = SparkSession.builder()
                 .appName("s-e")
                 .master("local[2]")
                 .getOrCreate()) {
+
+            spark.sparkContext().setLogLevel("INFO"); // TODO. Calculate in runtime.
 
             // TODO. Extract data.
 
@@ -44,14 +47,15 @@ public class Main {
                     new Record(3, "Aysh Talya ", "1991"),
                     new Record(4, "Bethany Euston", "1989"),
                     new Record(5, "Bethany Eustonn", "1989"),
-                    new Record(6, "Euston Bethany", "1989")
+                    new Record(6, "Bethan Eustonn", "1989"),
+                    new Record(7, "Euston Bethany", "1989")
             );
 
             final List<Row> data = new ArrayList<>();
 
             records.forEach((Record a) -> {
                 try {
-                    final String[] rawNames = a.name.split("\\s"); // TODO. Calculate into spark. And bday.
+                    final String[] rawNames = a.name.split("\\s"); // TODO. Calculate into spark. What about birthday and others?
                     final List<String> calculatedNames = new ArrayList<>();
                     final List<String> parseNames = new ArrayList<>();
                     for (String rName : rawNames) {
@@ -98,13 +102,13 @@ public class Main {
             final HashingTF hashingTF = new HashingTF()
                     .setInputCol("calculatedNames")
                     .setOutputCol("pFeatures")
-                    .setNumFeatures(30);
+                    .setNumFeatures(100);
 
             final Dataset<Row> fData = hashingTF.transform(dataSet);
 
-            final BucketedRandomProjectionLSH mh = new BucketedRandomProjectionLSH()
+            final BucketedRandomProjectionLSH mh = new BucketedRandomProjectionLSH() // TODO. Explain. Settings.
                     .setBucketLength(2.0)
-                    .setNumHashTables(3)
+                    .setNumHashTables(10)
                     .setInputCol("pFeatures")
                     .setOutputCol("values");
 
@@ -116,7 +120,7 @@ public class Main {
                     (Dataset<Row>) model.approxSimilarityJoin(transformedData, transformedData, 1.95);
 
             final StructType newKeysSchema = new StructType(new StructField[]{
-                    new StructField("key", DataTypes.StringType, true, Metadata.empty()),
+                    new StructField("newKey", DataTypes.StringType, true, Metadata.empty()),
                     new StructField("key1", DataTypes.IntegerType, true, Metadata.empty()),
                     new StructField("key2", DataTypes.IntegerType, true, Metadata.empty()),
                     new StructField("distance", DataTypes.DoubleType, false, Metadata.empty()),
@@ -134,19 +138,49 @@ public class Main {
                 return RowFactory.create(newKey, key1, key2, distance);
             }, RowEncoder.apply(newKeysSchema));
 
-            final Dataset<Row> pureKeysData = newKeysData.dropDuplicates("key");
+            final Dataset<Row> pureKeysData = newKeysData.dropDuplicates("newKey");
 
-            pureKeysData.select("key1", "key2").write().option("delimiter", " ").csv("graph.csv");
+            final Dataset<Row> filteredPureKeysData = pureKeysData.filter((FilterFunction<Row>) row -> {
+                final int key1 = row.getAs(1);
+                final int key2 = row.getAs(2);
+                final double distance = row.getDouble(3);
+
+                return distance <= 1.95 && key1 != key2;
+            });
+
+            filteredPureKeysData.select("key1", "key2").write().option("delimiter", " ").csv("graph");
 
             final Graph<Object, Object> graph = GraphLoader.edgeListFile(
                     spark.sparkContext(),
-                    "graph.csv",
-                    true,
-                    0,
-                    StorageLevel.MEMORY_AND_DISK(),
-                    StorageLevel.MEMORY_AND_DISK());
+                    "graph",
+                    false,
+                    -1,
+                    StorageLevel.MEMORY_ONLY_SER_2(),
+                    StorageLevel.MEMORY_ONLY_SER_2());
 
-            System.out.println(graph.ops().removeSelfEdges().ops().collectNeighbors(EdgeDirection.Either()).count());
+            final StructType neighborsSchema = new StructType(new StructField[]{
+                    new StructField("nKey", DataTypes.IntegerType, true, Metadata.empty()),
+                    new StructField("nKeys", new ArrayType(DataTypes.IntegerType, true), false, Metadata.empty()),
+            });
+
+            final JavaRDD<Row> neighborsRows =
+                    graph.ops().collectEdges(EdgeDirection.Either()).toJavaRDD().map((Function<Tuple2<Object, Edge<Object>[]>, Row>) t -> {
+                        // TODO. Rewrite.
+                        final Long key = (Long) t._1;
+                        final Set<Integer> keys = new TreeSet<>();
+                        for (int i = 0; i < t._2.length; i++) {
+                            final Edge<Object> objectEdge = t._2[i];
+                            keys.add((int) objectEdge.srcId());
+                            keys.add((int) objectEdge.dstId());
+                        }
+                        return RowFactory.create(key.intValue(), keys.toArray(new Integer[keys.size()]));
+                    });
+
+            final Dataset<Row> allKeysData = spark.createDataFrame(neighborsRows, neighborsSchema);
+
+            dataSet.join(allKeysData, dataSet.col("key").equalTo(allKeysData.col("nKey")), "fullouter").show(100);
+
+            // TODO. Save.
         }
     }
 }
