20739230b78c9b79640b454bd2b7deffaebc6315
akoval <c@apple456>
2019-03-05 09:41:55 +0300

    d-e. Trees.

--- a-m/src/main/java/m/d/a/m/p/trees/Trees.java
+++ a-m/src/main/java/m/d/a/m/p/trees/Trees.java
@@ -1,5 +1,7 @@
 package m.d.a.m.p.trees;
 
+import java.util.LinkedList;
+
 public class Trees {
 
     public static void main(final String[] args) {
@@ -19,18 +21,18 @@ public class Trees {
     public static class BinaryTreeNode {
         TreeNode root;
 
-        public void add(int value) {
-            root = add(root, value);
+        public void add(int val) {
+            root = add(root, val);
         }
 
-        private TreeNode add(TreeNode current, int value) {
+        private TreeNode add(TreeNode current, int val) {
             if (current == null) {
-                return new TreeNode(value);
+                return new TreeNode(val);
             }
-            if (value < current.val) {
-                current.left = add(current.left, value);
-            } else if (value > current.val) {
-                current.right = add(current.right, value);
+            if (val < current.val) {
+                current.left = add(current.left, val);
+            } else if (val > current.val) {
+                current.right = add(current.right, val);
             } else {
                 return current;
             }
@@ -44,6 +46,57 @@ public class Trees {
                 traverseInOrder(node.right);
             }
         }
+
+        public void traversePreOrder(TreeNode node) {
+            if (node != null) {
+                System.out.println(node.val);
+                traverseInOrder(node.left);
+                traverseInOrder(node.right);
+            }
+        }
+
+        public void traversePostOrder(TreeNode node) {
+            if (node != null) {
+                traverseInOrder(node.left);
+                traverseInOrder(node.right);
+                System.out.println(node.val);
+            }
+        }
+
+        public boolean contains(int val) {
+            return contains(root, val);
+        }
+
+        private boolean contains(TreeNode current, int val) {
+            if (current == null) {
+                return false;
+            }
+            if (current.val == val) {
+                return true;
+            }
+            return val < current.val
+                    ? contains(current.left, val)
+                    : contains(current.right, val);
+        }
+
+        public void traverseLevelOrder() {
+            if (root == null) {
+                return;
+            }
+            final LinkedList<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                final TreeNode node = queue.remove();
+                System.out.println(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+        }
     }
 
     static void test() {
@@ -53,7 +106,18 @@ public class Trees {
         binaryTreeNode.add(8);
         binaryTreeNode.add(10);
         binaryTreeNode.add(12);
+        System.out.println("--1--");
         binaryTreeNode.traverseInOrder(binaryTreeNode.root);
+        System.out.println("--2--");
         System.out.println(binaryTreeNode.root.val);
+        System.out.println("--3--");
+        System.out.println(binaryTreeNode.contains(8));
+        System.out.println(binaryTreeNode.contains(14));
+        System.out.println("--4--");
+        binaryTreeNode.traversePreOrder(binaryTreeNode.root);
+        System.out.println("--5--");
+        binaryTreeNode.traversePostOrder(binaryTreeNode.root);
+        System.out.println("--6--");
+        binaryTreeNode.traverseLevelOrder();
     }
 }
