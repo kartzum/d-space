31de904b678d1bdc2103a282b955c834c3e78768
akoval <c@apple456>
2019-04-03 09:48:05 +0300

    lt. StrStr.

--- a-m/src/main/java/m/d/a/m/p/lt/problems/ImplementStrStr.java
+++ a-m/src/main/java/m/d/a/m/p/lt/problems/ImplementStrStr.java
@@ -44,4 +44,16 @@ public class ImplementStrStr {
             return -1;
         }
     }
+
+    static class Solution2 {
+        public int strStr(String haystack, String needle) {
+            for (int i = 0; ; i++) {
+                for (int j = 0; ; j++) {
+                    if (j == needle.length()) return i;
+                    if (i + j == haystack.length()) return -1;
+                    if (needle.charAt(j) != haystack.charAt(i + j)) break;
+                }
+            }
+        }
+    }
 }
