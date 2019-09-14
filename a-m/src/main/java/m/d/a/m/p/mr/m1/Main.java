73a3ec6bb0445b290d0bfacfcbd37d5177b57159
kartzum <kartzum@gmail.com>
2019-09-14 17:50:51 +0300

    a-m. m1.

---
+++ a-m/src/main/java/m/d/a/m/p/mr/m1/Main.java
@@ -0,0 +1,19 @@
+package m.d.a.m.p.mr.m1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.nextLine();
+        String d[] = scanner.nextLine().split(" ");
+        List<String> list = Arrays.asList(d);
+        long r = 0;
+        for (String s : list) {
+            long l = Long.parseLong(s);
+            r += l;
+        }
+        System.out.println(r);
+        scanner.close();
+    }
+}
