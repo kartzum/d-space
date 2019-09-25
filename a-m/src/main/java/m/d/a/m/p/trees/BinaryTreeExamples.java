dbe0cca159d4a855302508806c62c2fb2471c4cb
akoval <c@apple456>
2019-09-25 10:35:48 +0300

    a-m. Binary Tree.

--- a-m/src/main/java/m/d/a/m/p/trees/BinaryTreeExamples.java
+++ a-m/src/main/java/m/d/a/m/p/trees/BinaryTreeExamples.java
@@ -2,16 +2,44 @@ package m.d.a.m.p.trees;
 
 public class BinaryTreeExamples {
     public static void main(final String[] args) {
-        test1();
+        // test1();
+        // test2();
+        test3();
     }
 
     private static void test1() {
+        BinaryTree binaryTree = create1();
+        binaryTree.inOrder();
+    }
+
+    private static void test2() {
+        BinaryTree binaryTree = create1();
+        // BinaryTree binaryTree1 = new BinaryTree();
+        TreeNode node = binaryTree.find(6);
+        if (node != null) {
+            System.out.println(node.val);
+        }
+    }
+
+    private static void test3() {
+        BinaryTree binaryTree = create1();
+        TreeNode min = binaryTree.min();
+        if (min != null) {
+            System.out.println(min.val);
+        }
+        TreeNode max = binaryTree.max();
+        if (max != null) {
+            System.out.println(max.val);
+        }
+    }
+
+    private static BinaryTree create1() {
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.insert(5);
         binaryTree.insert(2);
         binaryTree.insert(3);
         binaryTree.insert(6);
-        binaryTree.inOrder();
+        return binaryTree;
     }
 
     private static class TreeNode {
@@ -51,6 +79,38 @@ public class BinaryTreeExamples {
                     }
                 }
             }
+        }
+
+        TreeNode find(int val) {
+            TreeNode current = root;
+            while (current != null && current.val != val) {
+                if (val < current.val) {
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+            }
+            return current;
+        }
+
+        TreeNode min() {
+            TreeNode current = root;
+            TreeNode last = null;
+            while (current != null) {
+                last = current;
+                current = current.left;
+            }
+            return last;
+        }
+
+        TreeNode max() {
+            TreeNode current = root;
+            TreeNode last = null;
+            while (current != null) {
+                last = current;
+                current = current.right;
+            }
+            return last;
         }
 
         void inOrder() {
