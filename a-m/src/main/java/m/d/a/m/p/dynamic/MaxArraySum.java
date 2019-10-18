8ac692ac6398935de700c5a9608f215a83a373b3
akoval <c@apple456>
2019-10-18 13:57:57 +0300

    a-m. MaxArraySum.

--- a-m/src/main/java/m/d/a/m/p/dynamic/MaxArraySum.java
+++ a-m/src/main/java/m/d/a/m/p/dynamic/MaxArraySum.java
@@ -18,16 +18,13 @@ public class MaxArraySum {
 
     static class Solution1 {
         int calc(int[] arr) {
-            int s = arr[0];
-            int prev = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                int prevOld = prev;
-                prev = Math.max(prev, s);
-                s = arr[i] + prevOld;
-            }
-
-            return Math.max(s, prev);
+            if (arr.length == 0) return 0;
+            arr[0] = Math.max(0, arr[0]);
+            if (arr.length == 1) return arr[0];
+            arr[1] = Math.max(arr[0], arr[1]);
+            for (int i = 2; i < arr.length; i++)
+                arr[i] = Math.max(arr[i - 1], arr[i] + arr[i - 2]);
+            return arr[arr.length - 1];
         }
     }
 
