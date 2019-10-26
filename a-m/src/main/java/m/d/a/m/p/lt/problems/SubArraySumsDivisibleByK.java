258c546ea83e5c671f2d3b62f8e35778dd39579d
kartzum <kartzum@gmail.com>
2019-10-26 13:26:46 +0300

    a-m. 974.

--- a-m/src/main/java/m/d/a/m/p/lt/problems/SubArraySumsDivisibleByK.java
+++ a-m/src/main/java/m/d/a/m/p/lt/problems/SubArraySumsDivisibleByK.java
@@ -1,5 +1,7 @@
 package m.d.a.m.p.lt.problems;
 
+import java.util.HashMap;
+
 public class SubArraySumsDivisibleByK {
     public static void main(final String[] args) {
         test();
@@ -7,6 +9,7 @@ public class SubArraySumsDivisibleByK {
 
     static void test() {
         System.out.println(new Solution().subarraysDivByK(new int[]{4, 5, 0, -2, -3, 1}, 5));
+        System.out.println(new Solution2().subarraysDivByK(new int[]{4, 5, 0, -2, -3, 1}, 5));
     }
 
     static class Solution {
@@ -25,5 +28,25 @@ public class SubArraySumsDivisibleByK {
                 ans += v * (v - 1) / 2;
             return ans;
         }
+    }
+
+    static class Solution2 {
+        public int subarraysDivByK(int[] A, int K) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            int runningSum = 0;
+            int count = 0;
+            for (int i = 0; i < A.length; i++) {
+                runningSum += A[i];
+                int remainder = runningSum % K;
+                if (remainder < 0) remainder += K;
+                if (map.containsKey(remainder)) {
+                    count += map.get(remainder);
+                }
+                map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+            }
+            return count;
+        }
+
     }
 }
