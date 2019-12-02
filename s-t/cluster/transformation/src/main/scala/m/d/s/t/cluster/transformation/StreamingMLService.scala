e4f9805bc3f9fc7d6ddd489d928285bf7cf395b0
akoval <c@apple456>
2019-12-02 13:19:14 +0300

    s-t. Fix.

--- s-t/cluster/transformation/src/main/scala/m/d/s/t/cluster/transformation/StreamingMLService.scala
+++ s-t/cluster/transformation/src/main/scala/m/d/s/t/cluster/transformation/StreamingMLService.scala
@@ -1,9 +1,10 @@
 package m.d.s.t.cluster.transformation
 
+import org.apache.spark.sql.Row
 import org.apache.spark.streaming.dstream.InputDStream
 
-object StreamingMLService {
-  def predict(dataFrame: InputDStream[String]): Unit = {
+class StreamingMLService {
+  def predict(dataFrame: InputDStream[Row]): Unit = {
     dataFrame.foreachRDD(r => {
       r.foreach(s => {
         println(s)
--- s-t/cluster/transformation/src/test/scala/m/d/s/t/cluster/transformation/StreamingMLServiceSuite.scala
+++ s-t/cluster/transformation/src/test/scala/m/d/s/t/cluster/transformation/StreamingMLServiceSuite.scala
@@ -2,7 +2,7 @@ package m.d.s.t.cluster.transformation
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoSerializer
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.scalatest.{Matchers, Outcome}
@@ -14,15 +14,19 @@ class StreamingMLServiceSuite extends org.scalatest.fixture.FunSuite with Matche
     spark =>
       val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
 
-      val predictData = mutable.Queue[RDD[String]]()
-      val predictDStream: InputDStream[String] = ssc.queueStream(predictData)
+      val predictData = mutable.Queue[RDD[Row]]()
+      val predictDStream: InputDStream[Row] = ssc.queueStream(predictData)
 
-      StreamingMLService.predict(predictDStream)
+      val service = new StreamingMLService()
+      service.predict(predictDStream)
 
       ssc.start()
 
-      predictData += spark.sparkContext.makeRDD(Seq("a b c d"))
-      predictData += spark.sparkContext.makeRDD(Seq("e f"))
+      val d1 = spark.sparkContext.makeRDD(Seq(Row(0.1, 0.2, 0.3)))
+      val d2 = spark.sparkContext.makeRDD(Seq(Row(0.3, 0.1, 0.2)))
+
+      predictData += d1
+      predictData += d2
 
       ssc.awaitTerminationOrTimeout(5000)
   }
