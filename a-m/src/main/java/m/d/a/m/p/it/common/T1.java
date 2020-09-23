ec642b212c53d0b218672a3a9faa14012dbea736
kartzum <kartzum@gmail.com>
2020-09-23 11:08:55 +0300

    a-m.

---
+++ a-m/src/main/java/m/d/a/m/p/it/common/T1.java
@@ -0,0 +1,32 @@
+package m.d.a.m.p.it.common;
+
+public class T1 {
+    public static void main(String[] args) {
+        maxConTest();
+    }
+
+    // Maximum consecutive one’s (or zeros) in a binary array
+    // https://www.geeksforgeeks.org/maximum-consecutive-ones-or-zeros-in-a-binary-array/
+
+    static void maxConTest() {
+        System.out.println("2 = " + getMaxLength(new int[]{1, 1, 0, 1, 1, 0}));
+        System.out.println("3 = " + getMaxLength(new int[]{1, 1, 0, 1, 1, 1}));
+        System.out.println("3 = " + getMaxLength(new int[]{1, 1, 1}));
+        System.out.println("0 = " + getMaxLength(new int[]{0, 0, 0}));
+        System.out.println("0 = " + getMaxLength(new int[]{}));
+    }
+
+    static int getMaxLength(int arr[]) {
+        int count = 0;
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count = 0;
+            } else {
+                count += 1;
+                result = Math.max(result, count);
+            }
+        }
+        return result;
+    }
+}
