07cd4a9f14c9005abe206f1d7dbcf24d4d7f012a
kartzum <kartzum@gmail.com>
2020-10-09 16:30:39 +0300

    a-с.

--- a-c/algoprog/ru/p74.py
+++ a-c/algoprog/ru/p74.py
@@ -1,10 +1,15 @@
 # https://algoprog.ru/material/p74 - comment.
+# https://notes.algoprog.ru/python_basics/5_float.html
+
+# from math import *
+
+eps = 1e-7
 
 a = float(input())
 b = float(input())
 c = float(input())
 
-if round(a + b, 7) == c:
+if abs(a + b - c) < eps:
     print("YES")
 else:
     print("NO")
