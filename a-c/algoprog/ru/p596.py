cbb70d7742c401ff78408e28531e88c428d46bd6
kartzum <kartzum@gmail.com>
2020-10-09 17:36:39 +0300

    a-с.

--- a-c/algoprog/ru/p596.py
+++ a-c/algoprog/ru/p596.py
@@ -1,18 +1,41 @@
 # https://algoprog.ru/material/p596 - comment.
 
-x = int(input())
-y = int(input())
+debug = False
+prod = True
+test = False
+
+eps = 1e-7
+
+
+def read():
+    input_values = input().split()
+    x = float(input_values[0])
+    y = float(input_values[1])
+    if debug:
+        print(x, y)
+    return x, y
 
 
 def c(x, y):
-    k = 2
-    while (True):
-        if x + x * 0.7 <= y:
-            x += x * 0.7
-            k += 1
-        else:
-            break
+    k = 1
+    while x <= y + eps:
+        x += x * .7
+        k += 1
     return k
 
 
-print(c(10, 50))
+def tests():
+    # print(10, 10 + 10 * 0.7, 17.0 + 17.0 * 0.7, 28.9 + 28.9 * 0.7)
+    # assert c(10, 30) == 4
+    # assert c(10, 50) == 5
+    # assert c(10, 10) == 1
+    # assert c(10, 20) == 3
+    assert c(15.5, 35.5) == 3
+
+
+if test:
+    tests()
+
+if prod:
+    x, y = read()
+    print(c(x, y))
