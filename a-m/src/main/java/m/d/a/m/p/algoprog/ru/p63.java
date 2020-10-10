08222d7eadff4f73f74f135aacba94f74b2de441
kartzum <kartzum@gmail.com>
2020-10-10 14:09:53 +0300

    a-m.

---
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p63.java
@@ -0,0 +1,19 @@
+// https://algoprog.ru/material/p63 - in progress.
+
+package m.d.a.m.p.algoprog.ru;
+
+import java.util.Scanner;
+
+public class p63 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String n = input.nextLine();
+        String values = input.nextLine();
+        String[] nums = values.split(" ");
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(nums[i] + " ");
+            }
+        }
+    }
+}
