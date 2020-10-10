77b8d2ea32665b8d82fb1462ba055a95fa624ade
kartzum <kartzum@gmail.com>
2020-10-10 12:38:07 +0300

    a-m.

---
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p102.java
@@ -0,0 +1,19 @@
+// https://algoprog.ru/material/p102 - in progress.
+
+package m.d.a.m.p.algoprog.ru;
+
+import java.util.Scanner;
+
+public class p102 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String value = input.nextLine();
+        char c = value.charAt(0);
+        if (c >= '0' && c <= '9') {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
+    }
+}
