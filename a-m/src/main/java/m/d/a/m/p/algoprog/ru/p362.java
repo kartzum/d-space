d5b46dd3e4d1ae9e70825b04e2bc77651072a008
kartzum <kartzum@gmail.com>
2020-10-11 22:03:06 +0300

    a-m.

--- a-m/src/main/java/m/d/a/m/p/algoprog/ru/p362.java
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p362.java
@@ -19,7 +19,7 @@ public class p362 {
         }
         for (int j = 1; j < n; j++) {
             for (int i = 1; i < m; i++) {
-                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+                matrix[j][i] = matrix[j - 1][i] + matrix[j][i - 1];
             }
         }
         for (int j = 0; j < n; j++) {
