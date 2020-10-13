45004db1bae19de0b9324c8ed4af55e5d6f1746d
kartzum <kartzum@gmail.com>
2020-10-13 20:43:36 +0300

    a-m.

---
+++ a-c/algoprog/ru/p303.py
@@ -0,0 +1,10 @@
+# https://algoprog.ru/material/p264 - in progress.
+
+n = int(input())
+
+if n % 10 == 1 and n != 11:
+    print(n, "korova")
+elif 2 <= n % 10 <= 4 and n // 10 != 1:
+    print(n, "korovy")
+else:
+    print(n, "korov")
