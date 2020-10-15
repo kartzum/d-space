6c32ec3b54d0fc8634f1516057eed74f193354e0
kartzum <kartzum@gmail.com>
2020-10-15 21:36:51 +0300

    a-c.

--- a-c/algoprog/ru/p264.py
+++ a-c/algoprog/ru/p264.py
@@ -4,13 +4,9 @@ n = int(input())
 
 
 def check(n):
-    if n <= 2 or n == 4 or n == 7:
-        return "NO"
-    v3 = n % 3
-    v5 = n % 5
-    if v3 == v5 == 2:
-        return "NO"
-    return "YES"
+    if n != 0 and n != 1 and n != 2 and n != 4 and n != 7:
+        return "YES"
+    return "NO"
 
 
 print(check(n))
