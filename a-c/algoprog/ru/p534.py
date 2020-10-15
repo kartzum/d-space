138046f7a74e9aa2409adbde1492ae4193d854a3
kartzum <kartzum@gmail.com>
2020-10-15 21:29:57 +0300

    a-c.

--- a-c/algoprog/ru/p534.py
+++ a-c/algoprog/ru/p534.py
@@ -1,6 +1,5 @@
 # https://algoprog.ru/material/p506 - in progress.
 
 k, t = map(int, input().split())
-n = t % (2 * k)
-if n > k: n = 2 * k - n
-print(n)
+temp = (t - 1) % (2 * k)
+print(k - abs(temp - k + 1))
