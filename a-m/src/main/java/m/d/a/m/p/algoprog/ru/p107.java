25629c976ec55f2c8d4a9b6520ae8b48ea021492
kartzum <kartzum@gmail.com>
2020-10-17 15:32:49 +0300

    a-m.

---
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p107.java
@@ -0,0 +1,31 @@
+package m.d.a.m.p.algoprog.ru;
+
+import java.util.Scanner;
+
+public class p107 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        int l = 0;
+        int r = 0;
+        String maxValue = "";
+        while (r < a.length()) {
+            if (a.charAt(r) == ' ') {
+                String temp = a.substring(l, r);
+                if (temp.length() > maxValue.length()) {
+                    maxValue = temp;
+                }
+                l = r + 1;
+            }
+            r++;
+        }
+        if (l < r) {
+            String temp = a.substring(l, r);
+            if (temp.length() > maxValue.length()) {
+                maxValue = temp;
+            }
+        }
+        System.out.println(maxValue);
+        System.out.println(maxValue.length());
+    }
+}
