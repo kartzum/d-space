2e9995bbbeac29928f4e7a10745a1d71ce129e69
kartzum <kartzum@gmail.com>
2020-10-17 15:09:19 +0300

    a-c.

--- a-c/algoprog/ru/p235.py
+++ a-c/algoprog/ru/p235.py
@@ -3,11 +3,13 @@
 a, b, c = map(int, input().split())
 
 if a == 0 and c >= 0:
-    print("NO SOLUTION")
+    if c * c == b:
+        print("MANY SOLUTIONS")
+    else:
+        print("NO SOLUTION")
 elif a == 0 and c < 0:
     print("NO SOLUTION")
-elif c >= 0:
-    x = (c * c - b) // a
-    print(x)
+elif ((c * c - b) % a == 0) and (c >= 0):
+    print((c * c - b) // a)
 else:
     print("NO SOLUTION")
