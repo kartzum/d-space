4b206d84069e5a7856c148fffb28f0703e4a2ff8
kartzum <kartzum@gmail.com>
2020-10-19 11:11:13 +0300

    a-m.

---
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p3477.java
@@ -0,0 +1,16 @@
+// https://algoprog.ru/material/p3477 - in progress.
+
+package m.d.a.m.p.algoprog.ru;
+
+import java.util.Scanner;
+
+public class p3477 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int h = input.nextInt();
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int v = ((h - b - 1) / (a - b)) + 1;
+        System.out.println(v);
+    }
+}
