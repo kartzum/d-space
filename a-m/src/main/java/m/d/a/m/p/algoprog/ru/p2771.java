0fef3be04c3b2502da9470638f571c4a7bc4ebd4
kartzum <kartzum@gmail.com>
2020-11-11 10:03:22 +0300

    a-m.

--- a-m/src/main/java/m/d/a/m/p/algoprog/ru/p2771.java
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/p2771.java
@@ -2,16 +2,18 @@
 
 package m.d.a.m.p.algoprog.ru;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class p2771 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String[] nm = input.nextLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = reader.readLine().split(" ");
         int n = Integer.parseInt(nm[0]);
         int m = Integer.parseInt(nm[1]);
         int[] nums = new int[n];
-        String[] numsAsArray = input.nextLine().split(" ");
+        String[] numsAsArray = reader.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(numsAsArray[i]);
         }
@@ -20,12 +22,16 @@ public class p2771 {
         for (int i = 1; i < n; i++) {
             pref[i] = pref[i - 1] + nums[i];
         }
+        StringBuilder buffer = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            String[] temp = input.nextLine().split(" ");
+            String[] temp = reader.readLine().split(" ");
             int l = Integer.parseInt(temp[0]) - 1;
             int r = Integer.parseInt(temp[1]) - 1;
-            System.out.println(query(pref, l, r));
+            // System.out.println(query(pref, l, r));
+            buffer.append(query(pref, l, r));
+            buffer.append("\n");
         }
+        System.out.println(buffer.toString());
     }
 
     static int query(int[] pref, int l, int r) {
