4f933679c69e273a75edad3960a200a0cace940e
kartzum <kartzum@gmail.com>
2020-11-11 09:57:42 +0300

    a-m.

--- a-m/src/main/java/m/d/a/m/p/algoprog/ru/pe2001p2.java
+++ a-m/src/main/java/m/d/a/m/p/algoprog/ru/pe2001p2.java
@@ -7,8 +7,6 @@ package m.d.a.m.p.algoprog.ru;
 import java.util.Scanner;
 
 public class pe2001p2 {
-    static char[] letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().toCharArray();
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
@@ -19,7 +17,7 @@ public class pe2001p2 {
         if (n == 0) {
             return "";
         } else {
-            return calc(n - 1) + letters[n - 1] + calc(n - 1);
+            return calc(n - 1) + (char) (65 + n - 1) + calc(n - 1);
         }
     }
 }
