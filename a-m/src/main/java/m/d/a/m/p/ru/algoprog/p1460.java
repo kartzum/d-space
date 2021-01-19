16ef5d56ebd14120f8855903fbba3d25de4dc6b8
kartzum <kartzum@gmail.com>
2021-01-19 23:17:06 +0300

    a-m.

--- a-m/src/main/java/m/d/a/m/p/ru/algoprog/p1460.java
+++ a-m/src/main/java/m/d/a/m/p/ru/algoprog/p1460.java
@@ -1,6 +1,9 @@
 // https://algoprog.ru/material/p1460
 
 // https://hsecodes.com/index.php/tasksdecision/index/467
+// https://www.geeksforgeeks.org/how-to-left-or-right-rotate-an-array-in-java/
+// https://www.geeksforgeeks.org/print-left-rotation-array/
+// https://leetcode.com/problems/rotate-array/
 
 package m.d.a.m.p.ru.algoprog;
 
@@ -22,6 +25,113 @@ public class p1460 {
     }
 
     static void calc(int n, int[] nn, int k) {
+        if (k > 0) {
+            rRotate(n, nn, k);
+        } else {
+            lRotate(n, nn, k);
+        }
+    }
+
+    static void rRotate(int n, int[] nn, int k) {
+        k %= nn.length;
+        reverse(nn, 0, nn.length - 1);
+        reverse(nn, 0, k - 1);
+        reverse(nn, k, nn.length - 1);
+        /*for (int i = 0; i < n; i++) {
+            System.out.print(nn[i] + " ");
+        }*/
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            buffer.append(nn[i]);
+            buffer.append(" ");
+        }
+        System.out.println(buffer.toString());
+    }
+
+    static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void lRotate(int n, int[] nn, int k) {
+        int kk = Math.abs(k);
+        int mod = kk % n;
+        /*for (int i = 0; i < n; ++i) {
+            System.out.print(nn[(i + mod) % n] + " ");
+        }*/
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            buffer.append(nn[(i + mod) % n]);
+            buffer.append(" ");
+        }
+        System.out.println(buffer.toString());
+    }
+
+    static void calc2(int n, int[] nn, int k) {
+        if (k > 0) {
+            rightRotate(n, nn, k);
+        } else {
+            leftRotate(n, nn, k);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(nn[i] + " ");
+        }
+    }
+
+    static void rightRotate(int n, int[] nn, int k) {
+        k = n - k;
+        k = k % n;
+        int i, j, k1, temp;
+        int g_c_d = gcd(k, n);
+        for (i = 0; i < g_c_d; i++) {
+            temp = nn[i];
+            j = i;
+            while (true) {
+                k1 = j + k;
+                if (k1 >= n)
+                    k1 = k1 - n;
+                if (k1 == i)
+                    break;
+                nn[j] = nn[k1];
+                j = k1;
+            }
+            nn[j] = temp;
+        }
+    }
+
+    static void leftRotate(int n, int[] nn, int k) {
+        k = k % n;
+        int i, j, k1, temp;
+        int g_c_d = gcd(k, n);
+        for (i = 0; i < g_c_d; i++) {
+            temp = nn[i];
+            j = i;
+            while (true) {
+                k1 = j + k;
+                if (k1 >= n)
+                    k1 = k1 - n;
+                if (k1 == i)
+                    break;
+                nn[j] = nn[k1];
+                j = k1;
+            }
+            nn[j] = temp;
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+
+    static void calc1(int n, int[] nn, int k) {
         StringBuilder buffer = new StringBuilder();
         if (k > 0) {
             int kk = k;
