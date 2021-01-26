8c126cab4f3198ac03c7082fd912e4b4a5aed5e3
kartzum <kartzum@gmail.com>
2021-01-26 18:59:02 +0300

    a-m.

--- a-c/ru/algoprog/p1009.py
+++ a-c/ru/algoprog/p1009.py
@@ -1,7 +1,8 @@
 # https://algoprog.ru/material/p1009
 
-n = int(input())
-
+# https://pastebin.com/sQctvtdL
+# https://infourok.ru/dlinnaya-arifmetika-na-c-opisanie-modeli-realizaciya-zadachi-1959820.html
+# https://ru.stackoverflow.com/questions/1184457/%D0%9F%D0%BE%D0%BC%D0%BE%D0%B3%D0%B8%D1%82%D0%B5-%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D1%83-%D0%A1%D1%82%D0%B5%D0%BF%D0%B5%D0%BD%D0%B8
 
 def f(n):
     result = []
@@ -17,20 +18,24 @@ def f(n):
     return result
 
 
-def calc(n):
-    lst = f(n)
-    s = set(lst)
-
-    num = 1
-    for i in s:
-        a = lst.count(i)
-        temp = i
-        while temp < a:
-            a = a % 2 + a // 2
-            temp *= i
-        num *= temp
-
-    return num
+x = int(input())
 
 
-print(calc(n))
+def calc(x):
+    if x == 1:
+        return 1
+    else:
+        a = list(set(f(x)))
+        y = 1
+        for i in range(len(a)):
+            y *= a[i]
+        if a[0] != x:
+            for i in range(40):
+                t = (i + 1) * y
+                if pow(t, t, x) == 0:
+                    return (i + 1) * y
+        else:
+            return a[0]
+
+
+print(calc(x))
