064764381e7f8c7acbfd83c3a125a02d7f19e466
kartzum <kartzum@gmail.com>
2021-02-17 21:58:59 +0300

    a-m.

--- a-m/src/main/java/m/d/a/m/p/ru/algoprog/p510.java
+++ a-m/src/main/java/m/d/a/m/p/ru/algoprog/p510.java
@@ -15,29 +15,32 @@ public class p510 {
 
     static void run() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int v = Integer.parseInt(reader.readLine());
         int[][] g = new int[v][v];
         int m = Integer.parseInt(reader.readLine());
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < v; i++) {
+            map.put(i, new HashSet<>());
+        }
         for (int i = 0; i < m; i++) {
             String[] t = reader.readLine().split(" ");
-            int k = Integer.parseInt(t[0]);
-            int[] ll = new int[k];
             for (int j = 0; j < t.length - 1; j++) {
-                ll[j] = Integer.parseInt(t[j + 1]);
-            }
-            for (int j = 0; j < ll.length; j++) {
-                int vv = ll[j] - 1;
+                int vv = Integer.parseInt(t[j + 1]) - 1;
                 g[i][vv] = 1;
+                g[vv][i] = 1;
+                map.get(vv).add(i);
             }
         }
         String[] t = reader.readLine().split(" ");
         int start = Integer.parseInt(t[0]) - 1;
         int end = Integer.parseInt(t[1]) - 1;
-        boolean[] visited = new boolean[v];
-        int dist[] = new int[v];
+        List<Integer> starts = new ArrayList<>(map.get(start));
+        List<Integer> ends = new ArrayList<>(map.get(end));
+        starts.sort(Comparator.naturalOrder());
+        ends.sort(Comparator.naturalOrder());
         // printG(v, g);
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        int r = bfs(v, g, start, end, visited, dist);
+        int r = bfs(v, g, starts, ends);
         System.out.println(r);
     }
 
@@ -54,25 +57,30 @@ public class p510 {
         int v = 5;
         int[][] g = new int[][]{
                 {1, 1, 0, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 0, 1, 0},
-                {0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0},
+                {1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1},
+                {0, 1, 0, 1, 1},
         };
-        boolean[] visited = new boolean[v];
-        int start = 0;
-        int end = 4;
-        int dist[] = new int[v];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        int r = bfs(v, g, start, end, visited, dist);
+        List<Integer> starts = new ArrayList<>();
+        starts.add(1);
+        starts.add(0);
+        List<Integer> ends = new ArrayList<>();
+        ends.add(4);
+        int r = bfs(v, g, starts, ends);
         System.out.println(r);
     }
 
-    static int bfs(int v, int[][] g, int start, int end, boolean[] visited, int dist[]) {
+    static int bfs(int v, int[][] g, List<Integer> starts, List<Integer> ends) {
         LinkedList<Integer> queue = new LinkedList<>();
-        queue.push(start);
-        visited[start] = true;
-        dist[start] = 0;
+        boolean[] visited = new boolean[v];
+        int dist[] = new int[v];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        for (int i : starts) {
+            queue.push(i);
+            visited[i] = true;
+            dist[i] = 0;
+        }
         while (!queue.isEmpty()) {
             int u = queue.remove();
             for (int i = 0; i < v; i++) {
@@ -83,9 +91,12 @@ public class p510 {
                 }
             }
         }
-        if (dist[end] == Integer.MAX_VALUE) {
-            return -1;
+        int min = Integer.MAX_VALUE;
+        for (int i : ends) {
+            if (dist[i] != Integer.MAX_VALUE && dist[i] < min) {
+                min = dist[i];
+            }
         }
-        return dist[end] - 1;
+        return min != Integer.MAX_VALUE ? min : -1;
     }
 }
