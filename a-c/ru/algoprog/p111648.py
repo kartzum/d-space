c87da3495443b761d1b2be8651c46e258016e924
kartzum <kartzum@gmail.com>
2021-03-10 12:14:31 +0300

    a-m.

--- a-c/ru/algoprog/p111648.py
+++ a-c/ru/algoprog/p111648.py
@@ -1,20 +1,20 @@
 # https://algoprog.ru/material/p111648
 
-m = 1
-k = 0
-n, *s = list(map(int,input().split())) + [10]
-while m!=k:
-    m=k
-    i = 0
-    while i < len(s) - 2:
-        if s[i]==s[i+1] and s[i]==s[i+2]:
-            k+=3
-            del s[i+2]
-            del s[i+1]
-            while s[i]==s[i+1]:
-                k+=1
-                del s[i+1]
-            del s[i]
-            break
-        i+=1
-print(k)
+import itertools
+
+n, *s = list(map(int, input().split()))
+
+min_len = 3
+
+
+def calc(s: list, res=0) -> int:
+    c = 0
+    for num, group in itertools.groupby(s):
+        lg = len(list(group))
+        c += lg
+        if lg >= min_len:
+            return calc(s[:c - lg] + s[c:], res + lg)
+    return res
+
+
+print(calc(s))
