90dbba9ef7d7c47806b9989b65c665d1a0cdce4d
kartzum <kartzum@gmail.com>
2024-02-17 15:25:11 +0300

    cc.

--- a-m/src/main/java/m/d/a/m/p/cc/Cc.java
+++ a-m/src/main/java/m/d/a/m/p/cc/Cc.java
@@ -13,7 +13,8 @@ public class Cc {
         // ArraysAndStrings.TwoPointersCombine.run();
         // ArraysAndStrings.SlidingWindowFindLength.run();
         // ArraysAndStrings.SlidingWindowFindLengthZero.run();
-        ArraysAndStrings.SlidingWindowNumSubarrayProductLessThanK.run();
+        // ArraysAndStrings.SlidingWindowNumSubarrayProductLessThanK.run();
+        ArraysAndStrings.PrefixSumAnswerQueries.run();
     }
 
     static class ArraysAndStrings {
@@ -159,6 +160,39 @@ public class Cc {
                     }
                     ans += right - left + 1;
                 }
+                return ans;
+            }
+        }
+
+        static class PrefixSumAnswerQueries {
+            static void run() {
+                System.out.println("answerQueries");
+                boolean[] ans = answerQueries(
+                        new int[]{1, 6, 3, 2, 7, 2},
+                        new int[][]{{0, 3}},
+                        13
+                );
+                for (boolean an : ans) {
+                    System.out.print(an + ",");
+                }
+                System.out.println();
+            }
+
+            static boolean[] answerQueries(int[] nums, int[][] queries, int limit) {
+                int[] prefix = new int[nums.length];
+                prefix[0] = nums[0];
+
+                for (int i = 1; i < nums.length; i++) {
+                    prefix[i] = prefix[i - 1] + nums[i];
+                }
+
+                boolean[] ans = new boolean[queries.length];
+                for (int i = 0; i < queries.length; i++) {
+                    int x = queries[i][0], y = queries[i][1];
+                    int curr = prefix[y] - prefix[x] + nums[x];
+                    ans[i] = curr < limit;
+                }
+
                 return ans;
             }
         }
