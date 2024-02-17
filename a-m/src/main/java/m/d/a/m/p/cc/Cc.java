3fe93c3d4389bfeca6c8eee13e5a0679c70e13b9
kartzum <kartzum@gmail.com>
2024-02-17 13:44:45 +0300

    cc.

--- a-m/src/main/java/m/d/a/m/p/cc/Cc.java
+++ a-m/src/main/java/m/d/a/m/p/cc/Cc.java
@@ -1,12 +1,16 @@
 package m.d.a.m.p.cc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/
  */
 public class Cc {
     public static void main(String[] args) {
-        ArraysAndStrings.TwoPointersIsPalindrome.run();
-        ArraysAndStrings.TwoPointersCheckForTarget.run();
+        //ArraysAndStrings.TwoPointersIsPalindrome.run();
+        //ArraysAndStrings.TwoPointersCheckForTarget.run();
+        ArraysAndStrings.TwoPointersCombine.run();
     }
 
     static class ArraysAndStrings {
@@ -50,6 +54,37 @@ public class Cc {
                     }
                 }
                 return false;
+            }
+        }
+
+        static class TwoPointersCombine {
+            static void run() {
+                System.out.println("Combine.");
+                combine(new int[]{1, 4, 7, 20}, new int[]{3, 5, 6}).forEach((i) -> System.out.print(i + ","));
+            }
+
+            static List<Integer> combine(int[] arr1, int[] arr2) {
+                List<Integer> ans = new ArrayList<>();
+                int i = 0;
+                int j = 0;
+                while (i < arr1.length && j < arr2.length) {
+                    if (arr1[i] < arr2[j]) {
+                        ans.add(arr1[i]);
+                        i++;
+                    } else {
+                        ans.add(arr2[j]);
+                        j++;
+                    }
+                }
+                while (i < arr1.length) {
+                    ans.add(arr1[i]);
+                    i++;
+                }
+                while (j < arr2.length) {
+                    ans.add(arr1[j]);
+                    j++;
+                }
+                return ans;
             }
         }
     }
